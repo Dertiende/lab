350db986cc53a78479aa11e6510e542a6b463ab3
Dertiende <47276572+Dertiende@users.noreply.github.com>
2020-03-07 13:09:40 +0300

    1

--- lab1.py
+++ lab1.py
@@ -1,123 +1,46 @@
 import random
-
-##import networkx as nx
-
-##g = nx.Graph()
-##nodes = 10
-##for i in range(nodes):
-##    g.add_node(i)
-##for i in range(nodes):
-##    b = random.randint(1, nodes+1)
-##    while i == b:
-    ##    b = random.randint(1, nodes)
-  ##  g.add_edge(i, b)
-##print(g.edges())
-##print(g.nodes())
-from typing import Dict, Any, Union
-
-graph: dict = {}
+import numpy as np
 
 
-def orient_generate_edges(graph,nodes): #weak
+def orient_generate_edges(nodes): #weak
+    matrix = np.zeros((nodes, nodes))
     for i in range(nodes):
-
-        for j in range (nodes):
-            onodedges=[]
-            numedges = random.randint(2, nodes)
-            #print("numedge", numedges)
-            lnodes = [q for q in range(nodes) if q!=i]
-            #print("q",random.choice(lnodes))
-            for k in range(numedges-1):
-                #print("''",k)
-                #print ("rr",p)
-                onodedges.append(random.choice(lnodes))
-                lnodes.remove(onodedges[k])
-            graph[i] = onodedges
-    return graph
+        for j in range(nodes):
+            ves = random.randint(0, 1)
+            if i != j:
+                matrix[i][j] = ves
+    return matrix
 
 
-def norient_generate_edges(graph,nodes): #strong
+def norient_generate_edges(nodes): #strong
+    matrix = np.zeros((nodes, nodes))
     for i in range(nodes):
+        for j in range(i + 1, nodes):
 
-        for j in range (nodes):
-            onodedges=[]
-            numedges = random.randint(2, nodes)
-            #print("numedge", numedges)
-            lnodes = [q for q in range(nodes) if q!=i]
-            #print("q",random.choice(lnodes))
-            for k in range(numedges-1):
+            ves = random.randint(0, 1)
+            if i != j:
+                matrix[i][j] = ves
+    return matrix
 
-                #print("''",k)
-                #print ("rr",p)
-                onodedges.append(random.choice(lnodes))
-                lnodes.remove(onodedges[k])
-            graph[i] = onodedges
-            print(graph)
-    for l in range(nodes):
-        for m in graph[l]:
-            #print("l",l)
-            #print("before", graph[m])
-            if graph[m].count(l)<1:
-                graph[m]= graph[m]+[l]
-            #print("after", graph[m])
-    return graph
-
-def orient_generate_edges_ves (graph,nodes): #weak
-    for i in range(nodes):
-
-        for j in range (nodes):
-            onodedges=[]
-            numedges = random.randint(2, nodes)
-            #print("numedge", numedges)
-            lnodes = [q for q in range(nodes) if q!=i]
-            #print("q",random.choice(lnodes))
-            for k in range(numedges-1):
-                #print("''",k)
-                #print ("rr",p)
-                a = ''
-                b =random.choice(lnodes)
-                a= a+ str(b)+' - {**1**}'
-                onodedges.append(a)
-                lnodes.remove(b)
-            graph[i] = onodedges
-    return graph
+def orient_generate_edges_ves (nodes): #weak
+        matrix = np.zeros((nodes, nodes))
+        for i in range(nodes):
+            for j in range(nodes):
+                ves = random.randint(0, 1)
+                if i != j and ves == 1:
+                    matrix[i][j] = random.randint(0, 1000)
+        return matrix
 
 
-def norient_generate_edges_ves(graph,nodes): #strong
-    ves =[]
-    for j in range (nodes):
-        ves.append([])
-        onodedges=[]
-        numedges = random.randint(2, nodes)
-        #print("numedge", numedges)
-        lnodes = [q for q in range(nodes) if q!=j]
-        #print("q",random.choice(lnodes))
-        for k in range(numedges-1):
+def norient_generate_edges_ves(nodes): #strong
+        matrix = np.zeros((nodes, nodes))
+        for i in range(nodes):
+            for j in range(i + 1, nodes):
 
-                #print("''",k)
-                #print ("rr",p)
-
-            b = random.choice(lnodes)
-            vesint = random.randint(1,50)
-            ves[j].append({b:vesint})
-            a = ''
-            a = a + str(b)+' *'+str(vesint)+'* '
-            onodedges.append([b,'*'+str(vesint)+'*'])
-            lnodes.remove(b)
-            print("lnodes", lnodes)
-        graph[j] = onodedges
-        print(graph)
-        print("ves", ves)
-    for l in range(nodes):
-        for m in range(len(graph[l])):
-            #print("l",l)
-            #print("before", graph[m])
-            #for k in range(len(graph[l].get(m))):
-
-            if graph[m][0] :
-                graph[m] = graph[m]+[l]
-            #print("after", graph[m])
-    return graph
+                ves = random.randint(0, 1)
+                if i != j and ves == 1:
+                    matrix[i][j] = random.randint(0, 1000)
+        return matrix
 
 #def ns_con_generate_edges(): #not stated
 #def generate_edges(graph):
@@ -131,4 +54,4 @@ def norient_generate_edges_ves(graph,nodes): #strong
             # if edge exists then append
             #edges.append((node, neighbour))
     #return edges
-print(norient_generate_edges_ves (graph,5))
+print(orient_generate_edges (10))
--- test.py
+++ test.py
@@ -1,11 +1,24 @@
 import random
+import numpy as np
 
 
-ves = []
+def orient_generate_edgessss(nodes):
+    matrix = np.zeros((nodes, nodes))
+    for i in range(nodes):
+        for j in range(nodes):
+            ves = random.randint(0,1)
+            if i != j and ves==1:
+                matrix[i][j]= random.randint(0,1000)
+    return matrix
 
-for i in range ( 10):
-    a = random.randint(1, 1000)
-    ves.append([])
-    for j in range (5):
-        ves[i].append({j:random.randint(1,100)})
-print (ves)
+def orient_generate_edges(nodes):
+    matrix = np.zeros((nodes, nodes))
+    for i in range(nodes):
+        for j in range(i+1, nodes):
+
+            ves = random.randint(0,1)
+            if i != j and ves==1:
+                matrix[i][j]= random.randint(0,1000)
+    return matrix
+
+print(orient_generate_edges(10))
